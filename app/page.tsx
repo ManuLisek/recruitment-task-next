c3e2ed5c30077b4b5792d60bbdbddecfb6f12508
ManuLisek <miclisowiec@gmail.com>
2025-03-05 14:23:33 +0100

    Create an axios config and fetch movies

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
-export default function Home() {
+import axiosConfig from '../api/axiosConfig'
+
+export default async function Home() {
+  const response = await axiosConfig.getMoviesData()
+  const movies = response.data.movies
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Main
+    <div className="container mx-auto">
+      <main className="flex justify-items-center items-center flex-wrap gap-4 w-full">
+        {movies.map((movie: any) => (
+          <div key={movie.key} className="flex-grow w-1/5 p-4 border rounded">
+            <h3 className="text-xl font-bold">{movie.title}</h3>
+            <p>{movie.overview}</p>
+            <img src={movie.poster.src} alt={movie.poster.alt} className="w-full max-w-sm" />
+          </div>
+        ))}
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="flex gap-6 flex-wrap items-center justify-center">
         Footer
       </footer>
     </div>
-  );
+  )
 }
