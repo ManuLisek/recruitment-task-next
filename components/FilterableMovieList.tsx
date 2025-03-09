54f264166335c720d4652a937a29b12acaa1c909
ManuLisek <miclisowiec@gmail.com>
2025-03-09 15:02:18 +0100

    Style movies list

--- components/FilterableMovieList.tsx
+++ components/FilterableMovieList.tsx
@@ -1,52 +1,75 @@
 "use client";
 
-import Link from 'next/link';
-import MovieCard from './MovieCard';
-import { TransformedMovie, Genre } from '@/types/movieTypes';
+import { useState } from "react";
+import Link from "next/link";
+import MenuIcon from "@mui/icons-material/Menu";
 import AdjustIcon from '@mui/icons-material/Adjust';
+import { TransformedMovie, Genre } from "@/types/movieTypes";
+import MovieCard from "./MovieCard";
 
 interface FilterableMovieListProps {
-    movies: TransformedMovie[];
-    genres: Genre[];
-    selectedGenreId?: number;
+  movies: TransformedMovie[];
+  genres: Genre[];
+  selectedGenreId?: number;
 }
 
 const FilterableMovieList = ({ movies, genres, selectedGenreId }: FilterableMovieListProps) => {
-    return (
-        <div className="flex flex-col md:flex-row gap-8">
-            <aside className="md:w-1/4">
-                <h2 className="text-xl font-bold mb-4">Genres</h2>
-                <ul className="space-y-2">
-                    <li>
-                        <Link href="/" className={`block ${!selectedGenreId ? "text-rose-600" : "text-sky-800"}`}>
-                            All
-                        </Link>
-                    </li>
-                    {genres.map((genre) => (
-                        <li key={genre.id}>
-                            <Link
-                                href={`/?genre=${genre.id}`}
-                                className={`block ${selectedGenreId === genre.id ? "text-rose-600" : "text-sky-800"}`}
-                            >
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-                                <span className="flex items-center">
-                                    <AdjustIcon fontSize="small" className="mr-1" />
-                                    {genre.name}
-                                </span>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </aside>
-            <section className="md:w-3/4">
-                <ul className="flex flex-wrap gap-4 list-none p-0">
-                    {movies.map((movie) => (
-                        <MovieCard key={movie.key} movie={movie} />
-                    ))}
-                </ul>
-            </section>
-        </div>
-    );
+  return (
+    <>
+      <div className="flex justify-center md:hidden mb-4">
+        <button onClick={() => setIsSidebarOpen((prev) => !prev)} className="p-2 border rounded flex items-center gap-2">
+          <MenuIcon fontSize="small" />
+          Genres
+        </button>
+      </div>
+      <div className="relative flex md:flex-row gap-8">
+        <aside
+          className={`
+                        fixed inset-y-0 left-0 z-20 w-3/4 p-4 bg-zinc-900 shadow transform transition-transform duration-300 ease-in-out 
+                        ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"} md:static md:w-1/4 md:translate-x-0 md:bg-transparent md:shadow-none
+                    `}
+        >
+          <h2 className="text-xl font-bold mb-4">Genres</h2>
+          <ul className="space-y-2">
+            <li>
+              <Link href="/" className={`block ${!selectedGenreId ? "text-rose-600" : "text-sky-800"}`}>
+                All
+              </Link>
+            </li>
+            {genres.map((genre) => (
+              <li key={genre.id}>
+                <Link
+                  href={`/?genre=${genre.id}`}
+                  className={`block ${selectedGenreId === genre.id ? "text-rose-600" : "text-sky-800"}`}
+                  onClick={() => setIsSidebarOpen(false)}
+                >
+                  <span className="flex items-center">
+                    <AdjustIcon fontSize="small" className="mr-1" />
+                    {genre.name}
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </aside>
+        {isSidebarOpen && (
+          <div
+            className="fixed inset-0 bg-black opacity-50 z-10 lg:hidden"
+            onClick={() => setIsSidebarOpen(false)}
+          />
+        )}
+        <section className="w-full lg:w-3/4 md:ml-auto">
+          <ul className="flex justify-center flex-wrap gap-4 list-none p-0">
+            {movies.map((movie) => (
+              <MovieCard key={movie.key} movie={movie} />
+            ))}
+          </ul>
+        </section>
+      </div>
+    </>
+  );
 };
 
 export default FilterableMovieList;
--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,32 +1,47 @@
 "use client";
 
 import Link from 'next/link';
-import {
-    Card, CardActionArea, CardMedia, CardContent, Typography, Box,
-} from '@mui/material';
+import { Card, CardActionArea, CardMedia, CardContent, Typography, Box } from '@mui/material';
 import RatingStars from '@/components/RatingStars';
 import { MovieCardProps } from '@/types/componentTypes';
+import BrokenImageIcon from '@mui/icons-material/BrokenImage';
 
 const MovieCard = ({ movie }: MovieCardProps) => {
+    const hasImage = !movie.poster.src.includes('w300null');
+
     return (
-        <Box
-            component="li"
-            sx={{
-                flexGrow: 1, maxWidth: '226px'
-            }}
-        >
+        <Box component="li" sx={{ flexGrow: 1, maxWidth: '226px' }}>
             <Link href={`/movie/${movie.key}`} passHref style={{ width: '100%', textDecoration: 'none' }}>
                 <Card sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-                    <CardActionArea sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1, }}>
-                        <CardMedia
-                            component="img"
-                            image={movie.poster.src}
-                            alt={movie.poster.alt}
-                            sx={{}}
-                        />
-                        <CardContent sx={{ textAlign: 'center', height: '120px' }}>
+                    <CardActionArea sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
+                        {hasImage ? (
+                            <CardMedia
+                                component="img"
+                                image={movie.poster.src}
+                                alt={movie.poster.alt}
+                            />
+                        ) : (
+                            <Box sx={{
+                                width: '226px',
+                                height: '100%',
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                backgroundColor: 'grey.300'
+                            }}>
+                                <BrokenImageIcon sx={{ fontSize: 64, color: 'grey.500' }} />
+                            </Box>
+                        )}
+                        <CardContent sx={{
+                            textAlign: 'center',
+                            height: '120px',
+                            minHeight: '120px',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'flex-start'
+                        }}>
                             <RatingStars rating={movie.voteAverage} />
-                            <Typography variant="h6" component="div">
+                            <Typography variant="subtitle1" component="div">
                                 {movie.title}
                             </Typography>
                         </CardContent>
